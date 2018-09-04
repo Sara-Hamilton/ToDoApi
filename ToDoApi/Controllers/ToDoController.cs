4b79d7ef14c4f95f27b626629cc09564116c7228
Sara <sara@indeterminate.net>
2018-09-04 15:18:08 -0700

    add toDoContext to controller

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+
+            if (_context.ToDoItems.Count() == 0)
+            {
+                //Create a new ToDoItem if collection is empty,
+                //Which means you can't delete all ToDoItems.
+                _context.ToDoItems.Add(new ToDoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+     }
+}
