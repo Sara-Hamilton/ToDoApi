129c7cf0198315c8aea6404fe05f77bf8f950729
Sara <sara@indeterminate.net>
2018-09-12 16:46:25 -0700

    add swagger

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ToDoApi
 {
@@ -13,10 +14,39 @@ namespace ToDoApi
             services.AddDbContext<ToDoContext>(opt =>
                 opt.UseInMemoryDatabase("ToDoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "To Do API",
+                    Version = "v1",
+                    Description = "A simple ToDo ASP.NET Core web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "To Do API",
+                        Email = "sara@sarahamilton.design",
+                        Url = "https://localhost:44350"
+                    }
+                });
+            });
         }
+
 
         public void Configure(IApplicationBuilder app)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseMvc();
